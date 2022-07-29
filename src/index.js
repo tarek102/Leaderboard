5260d194d6af9b8e66418df78f67f67484638393
Tarek <83262890+tarek102@users.noreply.github.com>
2022-07-29 16:48:28 +0200

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -2,41 +2,33 @@ import Leaderboard from './modules/Leaderboard';
 import Ui from './modules/Ui';
 import './style.css';
 
-
 // Variables
 
 const submitBtn = document.querySelector('#submit-btn');
 const inputScore = document.querySelector('#input-score');
 const inputName = document.querySelector('#input-name');
 const refreshBtn = document.querySelector('#refresh-btn');
-const list = document.querySelector('.score-list');
-
 const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/fgzmeCvOsXZ5bhSUpeKq/scores/';
 const leaderboard = new Leaderboard(url);
 const ui = new Ui();
 
-leaderboard.getData(url)
-
+leaderboard.getData(url);
 
 submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  leaderboard.addData(url, inputName.value, inputScore.value)
+  leaderboard.addData(url, inputName.value, inputScore.value);
   inputName.value = '';
   inputScore.value = '';
 
   setTimeout(() => {
-     leaderboard.getData(url);
-     const arr = JSON.parse(localStorage.getItem('list'));
-
+    leaderboard.getData(url);
   }, 1000);
-})
-
+});
 
 refreshBtn.addEventListener('click', () => {
-  
   leaderboard.getData(url);
-  if (localStorage.getItem('list')){
+  if (localStorage.getItem('list')) {
     const arr = JSON.parse(localStorage.getItem('list'));
     ui.display(arr);
   }
-})
+});
--- src/modules/Leaderboard.js
+++ src/modules/Leaderboard.js
@@ -4,24 +4,21 @@ export default class Leaderboard {
   }
 
   getData = async (url) => {
-    await fetch(url,{
+    await fetch(url, {
       method: 'GET',
     })
-    .then((res) => res.json())
-    .then((data) => {
-      localStorage.setItem('list', JSON.stringify(data.result));
-    })
+      .then((res) => res.json())
+      .then((data) => {
+        localStorage.setItem('list', JSON.stringify(data.result));
+      });
   };
-
-
 
   addData = async (url, user, score) => {
-    await fetch (url,{
+    await fetch(url, {
       method: 'POST',
-      headers: {'Content-Type' : 'application/json; charset=UTF-8'},
-      body: JSON.stringify({user: user, score}),
+      headers: { 'Content-Type': 'application/json; charset=UTF-8' },
+      body: JSON.stringify({ user, score }),
     })
-    .then((response) => response.json())
+      .then((response) => response.json());
   };
-
 }
